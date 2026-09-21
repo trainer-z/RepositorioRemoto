92902ef6bec28a2c32654a59fdb4358714098308
trainer-z <298007009+trainer-z@users.noreply.github.com>
2026-09-21 07:27:18 -0500

    feat: :sparkles: Agregue la funcion resta en calculadora.py
    En este caso hace la resta de 2 numeros

--- matematicas/calculadora.py
+++ matematicas/calculadora.py
@@ -2,3 +2,11 @@ def sumar_dos_numeros(numero1,numero2):
     print(numero1 + numero2)
 
 sumar_dos_numeros(5,2)
+
+def resta_dos_numeros():
+    n1=input("Ingresa el numero 1")
+    n2=input("Ingresa el numero 2")
+    print(n1-n2)
+
+resta_dos_numeros()
+
