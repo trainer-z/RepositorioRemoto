d1d032b7ad838ca58db118892c6c1d5c8ebeef6e
trainer-z <298007009+trainer-z@users.noreply.github.com>
2026-09-22 06:59:00 -0500

    feat: :sparkles: Edite el archivo calculadora.py
    En este caso hice la logica con librerias

--- operaciones/calculadora.py
+++ operaciones/calculadora.py
@@ -1,4 +1,14 @@
+import math
+
 # Se debe implementar la logica de potenciacion
+def potenciacion_astericos(base,exponente):
+    print(math.pow(base,exponente))
+
+potenciacion_astericos(5,2)
 
 
 #Se debe implementar la logica de raiz cuadrada
+def raiz_cuadrada_asteriscos(base):
+    print(math.sqrt(base))
+
+raiz_cuadrada_asteriscos(121)
