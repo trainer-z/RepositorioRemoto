26df419f5188f83e6e293bf6cfef1dca15c84b2f
trainer-z <298007009+trainer-z@users.noreply.github.com>
2026-09-22 06:56:27 -0500

    feat: :sparkles: Cree el archivo calculadora.py
    La logica es hallar la potenciacion y la raiz cuadrada

--- operaciones/calculadora.py
+++ operaciones/calculadora.py
@@ -1,4 +1,12 @@
 # Se debe implementar la logica de potenciacion
+def potenciacion_astericos(base,exponente):
+    print(base ** exponente)
+
+potenciacion_astericos(5,2)
 
 
 #Se debe implementar la logica de raiz cuadrada
+def raiz_cuadrada_asteriscos(base):
+    print(base ** 0.5)
+
+raiz_cuadrada_asteriscos(121)
